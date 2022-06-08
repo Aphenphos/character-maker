680a61c6fe048a98a0fd1853c02fb3c48b5b2882
Aphenphos <MaxwillWinters@gmail.com>
2022-06-08 16:56:37 -0400

    pictures change but phrases wont add

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// import needed modules
+// import needed modules does anything need to be imported?
 
 // state
 const character = {
@@ -43,7 +43,7 @@ const phraseButton = addPhraseSection.querySelector('button');
 
 function handleAddPhrase() {
     
-
+    const phrase = phraseInput.textContent; if (phrase) return;
     displayPhrases();
     phraseInput.value = '';
     phraseInput.focus();
@@ -64,10 +64,10 @@ const characterSection = document.getElementById('character-section');
 const [headImage, middleImage, pantsImage] = characterSection.querySelectorAll('img');
 
 function displayCharacter() {
-    // *** Set the src properties for the three images.
-    // You can use:
-    // 'assets/character/' + <get prop value here> + '-head.png'
-    // to format the correct file name
+    headImage.src = 'assets/character/' + character.head + '-head.png';
+    middleImage.src = 'assets/character/' + character.middle + '-middle.png';
+    pantsImage.src = 'assets/character/' + character.pants + '-pants.png';
+    // 'assets/character/' + <get prop value here> + '-head.png' update is working but images aren't
 }
 
 // Phrases
@@ -77,7 +77,11 @@ const phraseList = phrasesSection.querySelector('ul');
 function displayPhrases() {
     phraseList.innerHTML = '';
 
-    // ** Create an li for each phrase and append to the list
+    for (const phrase of character.phrases) {
+        const li = document.createElement('li');
+        li.textContent = phrase;
+        phraseList.add(li);
+    }
 }
 
 // page load actions
